b86e74036c6fd9aa854a1400afa77e154437c6a8
MARIA ALEJANDRA MORALES GARCIA <mmoralesg42@miumg.edu.gt>
2023-05-23 16:23:48 -0600

    le agregue el nombre de mi compu en la conexion por comentario

--- PROYF/CONEXION.h
+++ PROYF/CONEXION.h
@@ -19,7 +19,8 @@ protected:
     void Conectar() {
         st = gcnew SqlConnectionStringBuilder();
         //st->DataSource = "localhost\\SQLEXPRESS";
-        st->DataSource = "LAPTOP-C78Q6N2B\\SQLEXPRESS20";
+        st->DataSource = "VICTUSMARIO\\SQLEXPRESS";
+        //st->DataSource = "LAPTOP-C78Q6N2B\\SQLEXPRESS20";
         st->InitialCatalog = "proteccionTotal";
         st->IntegratedSecurity = true;
         cn = gcnew SqlConnection(Convert::ToString(st));
